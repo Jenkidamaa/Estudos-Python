4a6bca5af9278e6a5f3a7f98d1c1eaee6e4e4551
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-01-09 00:20:18 -0300

    Update Pessoas.py

--- Claases_Modulo/Pessoas.py
+++ Claases_Modulo/Pessoas.py
@@ -1,36 +1,51 @@
-#MOLDE
-class Pessoa:
-    def __init__(self, nome, idade, comendo =False,falando=False):
+from datetime import datetime
+
+class pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y')) #Variavel da classe
+
+    def __init__(self, nome, idade, comendo = False, falando = False):
         self.nome = nome
         self.idade = idade
         self.comendo = comendo
         self.falando = falando
-    def falar(self, assunto):
-        if self.comendo :
-            print(f"{self.nome} não pode falar comendo.")
-            return
-        if self.falando:
-            print(f"{self.nome} já esta falando.")
-        print(f"{self.nome} está falando sobre {assunto}")
-        self.falando = True
-    def parar_falar(self):
-        if not self.falando:
-            print(f"{self.nome} não esta falando.")
-            return
-        print(f"{self.nome} parou de falar.")
-        self.falando = False
+
+        self.variavel = "Valor"
 
     def comer(self, alimento):
         if self.comendo:
-            print(f"{self.nome} já está comendo.")
+            print(f'{self.nome} ja esta comendo')
             return
-        print(f"{self.nome} está comendo {alimento} .")
+        if self.falando:
+            print(f"{self.nome} não pode comer pois esta falando.")
+            return
+
+        print(f'{self.nome} esta comendo {alimento}.')
         self.comendo = True
-    #Variaveis de classe = atributos de classe
-    #Método é uma função que pertence a classe
     def parar_comer(self):
         if not self.comendo:
-            print(f"{self.nome} Nâo esta comendo.")
+            print(f"{self.nome} não esta comendo.")
             return
         print(f"{self.nome} parou de comer.")
         self.comendo = False
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f"{self.nome} ele não pode falar comendo")
+            return
+        if self.falando:
+            print(f"{self.nome} ja esta falando.")
+            return
+
+        print(f'{self.nome} esta falando sobre {assunto}. ')
+        self.falando = True
+    def parar_falar(self):
+        if not self.falando:
+            print(f'{self.nome} não esta falando.')
+            return
+
+        print(f"{self.nome} parou de falar.")
+        self.falando = False
+    def get_ano_nascimento(self):
+        
+        idade = self.ano_atual - self.idade
+        return print(f"o ano de nascimento é {idade}" )
