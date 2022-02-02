0aad33e9421172e2f9b1d802e0f55711fb5665d2
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-02-02 01:03:16 -0300

    Update Any or all.py

--- 110_exercicio_resolvidos/Any or all.py
+++ 110_exercicio_resolvidos/Any or all.py
@@ -1,13 +1,49 @@
-n = int(input()); l = str(input()); b = 0
-lista = list(str(n))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+palindromo = int(input())
+var = 0
+if 1 < palindromo <=10:
+  var += 1
+if 10 < palindromo < 100:
+  cs = str(palindromo)
+  if cs[0] == cs [1]:
+    var += 1
 
-if 0 < n < 10:
-  b += 1
-if 10 <= n < 100:
-  if lista [0] == lista[1]:
-    b += 1
+a = input()
+b = ""
+c = 0
+d= []
+f = 0
+while c<palindromo:
+  if f < len(a):
+    while a[f] != " " and f < len(a):
+      b += a[f]
+      f += 1
+      if f == len(a):
+          break
 
-lista2 = []
+  f += 1
+  d.append(b)
+  b = ""
+  c += 1 
 
-for i in range(0,len(l)):
-  print(l[i]+l[i+1])
+lista = []
+for i in d:
+  lista.append(int(i))
+i = len(lista)-1
+
+cont = 0
+while lista[i] >= 0 :
+    #print(lista[i])
+    if i == 0:
+      break
+    i -= 1
+    if i == 0:
+      break
+
+
+if i == 0:
+  var += 1
+if var == 2:
+  print(True)
+else:
+  print(False)
