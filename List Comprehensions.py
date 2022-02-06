3aebce2799d1c93b71c26f5cfdd8f8eda63272a7
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-02-06 20:01:21 -0300

    Create List Comprehensions.py

---
+++ List Comprehensions.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    for i in range(n):
+        a = [if((x+y+z)<=n and (0<=i<=x):  for (p) in range(x)]
+        b = [if((x+y+z)<=n and (0<=j<=y) for (o) in range(y)]
+        c = [if((x+y+z)<=n and (0<=k<=z) for (l) in range(z)]
+        m.append([a,b,c])
+        a [];b=[];c=[]
+print(m)               
