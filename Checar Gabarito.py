18ca4e64479c74330509eabc7204350901735bd5
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-02-16 12:02:13 -0300

    Create Checar Gabarito.py
    Programa que compara dois gabaritos , o do aluno e o oficial e retorna o numero de acertos.

---
+++ Checar Gabarito.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+gabarito_aluno = list(input())
+gabatiro_oficial = list(input())
+acu = 0
+for i in range(len(gabarito_aluno()):
+  if gabarito_aluno[i] == gabarito_oficial[i]:
+    acu += 1
+print(acu)
+               
+               
