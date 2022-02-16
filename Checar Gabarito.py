afb9e8cb9150605fdc4402b249abbae03ef857ff
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-02-16 12:12:57 -0300

    Update Checar Gabarito.py

--- Checar Gabarito.py
+++ Checar Gabarito.py
@@ -1,11 +1,9 @@
 n = int(input())
 
 gabarito_aluno = list(input())
-gabatiro_oficial = list(input())
+gabarito_oficial = list(input())
 acu = 0
-for i in range(len(gabarito_aluno()):
+for i in range(n):
   if gabarito_aluno[i] == gabarito_oficial[i]:
     acu += 1
 print(acu)
-               
-               
