80edbdeb791eeeafcc77b8581db1e89d7c9a1d8d
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-02-18 16:27:26 -0300

    Update segundo lugar.py

--- segundo lugar.py
+++ segundo lugar.py
@@ -1,18 +1,17 @@
-n = int(input())
-arr = map(int, input().split())
-
-
-
-maior = 0
-for i in arr:
-    if i > maior:
-        maior = i
-for i in range(len(arr)):
-    if i == maior:
-        arr.del(i)
-maior = 0
-for i in arr:
-    if i > maior:
-        maior = i
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    maior = 0
+    for i in arr:
+        if i > maior:
+            maior = i
+    for i in range(len(arr)):
+        if i == maior:
+            indice = arr.index(i)
+            del arr[indice]
+    maior = 0
+    for i in arr:
+        if i > maior:
+            maior = i
         
-print(maior)
+    print(maior)
