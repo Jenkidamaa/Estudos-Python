d1de0a0a1712e2be1b4a4d8504f59a7bac4b737e
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-01 23:22:06 -0300

    Update Criterios de Divisibilidade.py

--- Criterios de Divisibilidade.py
+++ Criterios de Divisibilidade.py
@@ -24,5 +24,19 @@ if int(n_div_4) % 4 == 0:
 if n_lista[len(n_lista-1)] == 0 or n_lista[len(n_lista-1)] == 5:
   print("Divisivel por 5")
   
-  
-  
+# 6 : Divisibilidade por 6 se um número for divisível por 2 e por 3.
+
+
+# 7 : Divisibilidade por 7, separar o número do seu último algarismo. Se o 1° grupo de algarismos separados menos o dobro do último 
+#algarismo for múltiplo de 7, então o número original é divisível por 7.
+
+n_div_7 = int(n_lista[len(n_lista-1)]
+for i in range(0,len(n_lista-2)):
+  n_div_7_primeiros += n_lista[i]
+n_div_7_primeiros_int = int(n_div_7_primeiros)
+if (n_div_7_primeiros_int - 2 * n_div_7) % 7 == 0:
+  print("Divisivel por 7")
+              
+              
+
+# 8 : Divisibilidade por 8 se os três últimos algarismos forem divisíveis por 8, (ou se terminar em 000) então o número também é divisível por 8
