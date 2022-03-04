3109aeae156e49b00c4ee21bfd0ad24ec3c4aff4
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-04 11:22:48 -0300

    Update Entradas de notas.py

--- funções/Entradas de notas.py
+++ funções/Entradas de notas.py
@@ -23,15 +23,48 @@ def armazena_nome(n): # = numero de alunos
 
 #Armazena apenas as notas 
 def armazena_notas(n,lista):
-    lita_notas = []
+    lista_notas = []
     acu = 0
     while acu < n:
         print("Insira a nota do aluno {} ".format(lista[acu])) #para esclarecer chama o nome do aluno 
         n1 = float(input("Digite a primeira nota "))
         n2 = float(input("Digite a segunda nota "))        
         lista_notas.append([n1,n2])
-    acu += 1
+        acu += 1
     return lista_notas   
     
+# função para tratar os dados calculando as medias, mediana e o desvio padrão das notas da turma
+# ∑: símbolo de somatório. Indica que temos que somar todos os termos, desde a primeira posição (i=1) até a posição n
+# xi: valor na posição i no conjunto de dados
+# MA: média aritmética dos dados
+# n: quantidade de dados
+
+def media(lista):
+    lista_media = []
+    for i in range(len(lista_media)):
+        lista_media.append(lista[i][0]+lista[i][1]) #n1+n2
+    return lista_media
+
+def mediana(lista):
+    lista_organizada = sorted(lista)
+    mediana = lista_organizada[int(len(lista_organizada/2))]
     
-        
+def desvio_padrão(n, lista_medias, ):
+    lista_medidas = lista_medias
+    passo = 0
+    for i in range(len(lista_medidas)):
+        media_1 += lista[i]
+    media = media_1 / (len(lista_medidas)+1)
+    
+    for i in range(len(lista_medidas)):
+        lista_medidas[i] = lista_medidas[i]-media
+        lista_medidas[i] = lista_medidas[i] ** 2
+        passo += lista_medidas[i]
+    
+    passo2 = passo / (len(lista_medidas)-1)
+    desvio = passo2 ** 1/2
+    return desvio   
+
+
+    
+    
