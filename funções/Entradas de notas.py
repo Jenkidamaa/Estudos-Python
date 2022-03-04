eae994f176dd94cfe461622f61261d838d016338
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-04 09:38:31 -0300

    Create Entradas de notas.py

---
+++ funções/Entradas de notas.py
@@ -0,0 +1,12 @@
+def armazena(n): #n = numero de alunos
+    lista_nome = []
+    lista_notas = []
+    acu = 1
+    while acu <= n:
+        lista_nome.append(str(input("Digite o nome do aluno de Ra de numero {}: ".format(acu))))
+        n1 = float(input("Digite a primeira nota "))
+        n2 = float(input("Digite a segunda nota "))
+        lista_notas.append([n1,n2])
+        acu += 1
+    return lista_nome, lista_notas
+    
