4f53b7ddd54b4061ddcb9dd84af44b2ec1577453
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-05 02:28:08 -0300

    Update finding-the-percentage.py

--- HackerRank/finding-the-percentage.py
+++ HackerRank/finding-the-percentage.py
@@ -8,3 +8,9 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    media = 0
+    media_1 = 0
+    for i in range(3):
+      media = student_marks[query_name][i]
+      media_1 += media
+    print("%.2f" %(media_1/3))
