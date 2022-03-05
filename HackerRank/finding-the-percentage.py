97b273ac4f1b462397c6bbdf82bd025c6cfa860a
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-05 01:56:17 -0300

    Create finding-the-percentage.py

---
+++ HackerRank/finding-the-percentage.py
@@ -0,0 +1,10 @@
+## https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
