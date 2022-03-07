463ca8ff3a9429e2ff48f903a0e6eebe7a2ded9f
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-07 17:37:02 -0300

    Create split_and_join.py

---
+++ HackerRank/split_and_join.py
@@ -0,0 +1,13 @@
+#You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+
+
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
