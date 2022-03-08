77725daabdafe86afddcd82ca3db17f69bdb29aa
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-08 11:05:26 -0300

    Update Estudo_Classes(Netflix).py

--- Classe/Estudo_Classes(Netflix).py
+++ Classe/Estudo_Classes(Netflix).py
@@ -1,33 +1,39 @@
-class Cliente:\n",
-  def __init__(self, nome, email,plano):\n",
-    self.nome = nome\n",
-    self.email = email\n",
-    self.lista_planos = ['Basico','Premiun']\n",
-    if plano in self.lista_planos:\n",
-      self.plano = plano\n",
-    else:\n",
-      raise Exception(\"Plano invalido\")\n",
-    \n",
-   def mudar_plano(self, nova_plano):\n",
-      if novo_plano in self.lista_planos:\n",
-        self.plano = novo_plano\n",
-      else:\n",
-        print(\"Plano invalido\")\n",
-   def ver_filme(self, filme, plano_filme):\n",
-      if self.plano == plano_filme:\n",
-        print(f\"Ver filme {filme}\")\n",
-      elif self.plano == \"premiun\":\n",
-        print(f\"Ver filme {filme}\")\n",
-      elif self.plano == \"basic\" and plano_filme == 'premiun':\n",
-        print(\"Faça upgrade para premiun para ver esse filme.\")\n",
-      else:\n",
-        print(\"Plano invalido.\")        \n",
+class cliente():
+    def __init__ (self, nome, email,plano):
+        self.nome = nome
+        self.email = email
+        self.plano = plano
+        self.lista_planos = ["basico","premium"]
+        if plano in self.lista_planos:
+            self.plano
+        else:
+            raise Exception("plano invalido")
 
-    #def ver_filme(self, filme):\n",
-cliente = Cliente(\"José Aldo\", \"abc@email.com\",\"Bx\" )\n",
 
-print(cliente.plano)\n",
+    def mudar_plano(self, novo_plano):
+        if novo_plano in self.lista_planos:
+            self.plano = novo_plano
+        else:
+            raise Exception("plano invalido")
 
-cliente.ver_filme(\"Fuga de Alcatraz\",\"Premiun\")\n",
-cliente.mudar_plano(\"Premiun\")\n",
-print(cliente.plano)"
+
+
+
+    def ver_filme(self,filme):
+        lista_filmes = ["A", "B", "C", "D", "E", "F"]
+        lista_filmes_basic = ["C", "D"]
+
+        if self.plano == "premium":
+            print("Plano Premium Acesso liberado ")
+        elif self.plano != "premium" or filme not in lista_filmes_basic:
+            print("Acesso Negado para esses filmes no plano basic")
+        else:
+            print("Plano Basic Acesso liberado")
+Cliente1 = cliente("XXXX","XXXX@hotmail.com", "basico")
+print(Cliente1.nome)
+print(Cliente1.plano)
+Cliente1.mudar_plano("premium")
+print(Cliente1.plano)
+Cliente1.ver_filme("A")
+Cliente1.mudar_plano("basico")
+Cliente1.ver_filme("A")
