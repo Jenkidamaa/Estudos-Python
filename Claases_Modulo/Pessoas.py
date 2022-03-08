7916a2d15793a726205b876a9477eff951e7856a
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-08 00:03:01 -0300

    Update Pessoas.py

--- Claases_Modulo/Pessoas.py
+++ Claases_Modulo/Pessoas.py
@@ -3,48 +3,57 @@ from datetime import datetime
 class pessoa:
     ano_atual = int(datetime.strftime(datetime.now(), '%Y')) #Variavel da classe
 
-    def __init__(self, nome, idade, comendo = False, falando = False):
-        self.nome = nome
-        self.idade = idade
-        self.comendo = comendo
-        self.falando = falando
-
-        self.variavel = "Valor"
-
+class Pessoa:
+    def __init__(self, nome, idade, comendo=False, falando=False, andar = False, parar = False, jogar = False, beber = False):
+         self.nome = nome
+         self.idade = idade
+         self.comendo = comendo
+         self.falando = falando
+         self.andar = andar
+         self.parar = parar
+         self.jogar = jogar
+         self.beber = beber
     def comer(self, alimento):
         if self.comendo:
-            print(f'{self.nome} ja esta comendo')
-            return
+          print(f"{self.nome} ja esta comendo.")
+          return
         if self.falando:
-            print(f"{self.nome} não pode comer pois esta falando.")
-            return
+          print(f"{self.nome} não pode comer pois esta falando.")
+          return
+     #codigo a ser executado caso não entre nos condicionais
 
-        print(f'{self.nome} esta comendo {alimento}.')
+        print(f"{self.nome} esta comendo {alimento}.")
         self.comendo = True
+
     def parar_comer(self):
         if not self.comendo:
-            print(f"{self.nome} não esta comendo.")
+            print(f"{self.nome} não esta comendo")
             return
         print(f"{self.nome} parou de comer.")
         self.comendo = False
 
     def falar(self, assunto):
         if self.comendo:
-            print(f"{self.nome} ele não pode falar comendo")
+            print(f"{self.nome} ele não pode falar comendo.")
             return
         if self.falando:
-            print(f"{self.nome} ja esta falando.")
+            print(f"{self.nome} ja esta falando sobre {self.assunto}.")
             return
-
-        print(f'{self.nome} esta falando sobre {assunto}. ')
+        print(f"{self.nome} esta falando sobre {self.assunto}.")
         self.falando = True
+
     def parar_falar(self):
         if not self.falando:
-            print(f'{self.nome} não esta falando.')
-            return
+            print(f"{self.falando} não esta falando.")
 
         print(f"{self.nome} parou de falar.")
-        self.falando = False
+        self.falando=False
+
+    def beber(self, bebida):
+
+
+        print(f"{self.nome} esta bebendo {self.bebida}")
+        self.bebendo = True
     def get_ano_nascimento(self):
         
         idade = self.ano_atual - self.idade
