13aa9a5aae231d862f6333052a099f853355ce9d
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-09 23:33:35 -0300

    Update Jogo_da_velha.py

--- Classe/Jogo_da_velha.py
+++ Classe/Jogo_da_velha.py
@@ -4,7 +4,6 @@ class jogo_da_velha:
         self.jogador2 = jogador2
         self.figura_jogador1 = jogador1
         self.figura_jogador2 = jogador2
-
     def tabuleiro(self):
         self.tabuleiro = [["-","-","-"],
                      ["-","-","-"],
@@ -14,7 +13,6 @@ class jogo_da_velha:
     def jogada(self, jogada1, jogada2):
         self.jogada1 = jogada1
         self.jogada2 = jogada2
-
     #def jogar(self,posição_1, posição_2):
     #    posição_1 =
 
