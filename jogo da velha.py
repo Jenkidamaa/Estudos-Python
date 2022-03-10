015b80a7b2bec3787bacd019fdb5e34c80a79e56
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-10 17:22:07 -0300

    Update jogo da velha.py

--- jogo da velha.py
+++ jogo da velha.py
@@ -5,34 +5,60 @@ def printar(tabuleiro):
     for i in range(len(tabuleiro)):
         print(tabuleiro[i])
 
-jogador1 = str(input())
-jogador2 = str(input())
-simbolo1 = str(input())
-simbolo2 = str(input())
+jogador1 = str(input("Insira o nome do primeiro jogador: "))
+jogador2 = str(input("Insira o nome do segundo jogador: "))
+simbolo1 = str(input("Escolha entre X ou O: "))
+if simbolo1 == "X":
+    simbolo2 = "O"
+else:
+    simbolo2 = "X"
+
+
 fim = False
+
+
 while fim != True:
     #entrada da coordenada
-    jogada_player_1 = tuple(input())
-    tabuleiro[jogada_player_1(0)][jogada_player_1(1)]
+    jogada_player_1_x = int(input())
+    jogada_player_1_y = int(input())
+    tabuleiro[jogada_player_1_x][jogada_player_1_y] = simbolo1
+
     #igualdade
+    #Varrer as linhas , colunas e diagonais em busca do padrão do jogador 1
+    printar(tabuleiro)
+    for i in range(3):
+        #linhas
+        if tabuleiro[i][0] == simbolo1 and tabuleiro[i][1] == simbolo1 and tabuleiro[i][2] == simbolo1:
+            fim = True
+            ganhador = jogador1
+        #colunas
+        elif tabuleiro[0][i] == simbolo1 and tabuleiro[1][i] == simbolo1 and tabuleiro[2][i] == simbolo1:
+            fim = True
+            ganhador = jogador1
+        #diagonal 1
+        elif tabuleiro[i][i] == simbolo1 and tabuleiro[i][i] == simbolo1 and tabuleiro[i][i] == simbolo1:
+            fim = True
+            ganhador = jogador1
+
+    jogada_player_2_x = int(input())
+    jogada_player_2_y = int(input())
+    tabuleiro[jogada_player_2_x][jogada_player_2_y] = simbolo2
+    #igualdade
+    #Varrer as linhas , colunas e diagonais em busca do padrão do jogador 2
 
     for i in range(3):
-        if tabuleiro[i][0] == tabuleiro[i][1] == tabuleiro[i][2] == simbolo1:
-            fim = True
-            ganhador = jogador1
-        elif tabuleiro[0][i] == tabuleiro[1][i] == tabuleiro[2][i] == simbolo1:
-            fim = True
-            ganhador = jogador1
-        elif tabuleiro[i][i] == tabuleiro[i][i] == tabuleiro[i][i] == simbolo1:
-            fim = True
-            ganhador = jogador1
-    jogada_player_2 = tuple(input())
-    for i in range(3):
-        if tabuleiro[i][0] == tabuleiro[i][1] == tabuleiro[i][2] == simbolo2:
-            fim = True
-            ganhador = jogador1
-        elif tabuleiro[0][i] == tabuleiro[1][i] == tabuleiro[2][i] == simbolo2:
+        if (tabuleiro[i][0] == simbolo2) and (tabuleiro[i][1] == simbolo2) and (tabuleiro[i][2] == simbolo2):
             fim = True
             ganhador = jogador2
+        elif (tabuleiro[0][i] == simbolo2) and (tabuleiro[1][i] == simbolo2) and (tabuleiro[2][i] == simbolo2):
+            fim = True
+            ganhador = jogador2
+        elif (tabuleiro[i][i] == simbolo2) and (tabuleiro[i][i] == simbolo2) and (tabuleiro[i][i] == simbolo2):
+            fim = True
+            ganhador = jogador2
+
     printar(tabuleiro)
+
+print(ganhador)
+
 
