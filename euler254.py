e40e6ede49bcfd4e757c7aadd2715ae07ea32a04
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-12 20:26:56 -0300

    Update euler254.py

--- euler254.py
+++ euler254.py
@@ -1,11 +1,6 @@
-#Define  as the sum of the factorials of the digits of . For example, .
-
+#Define  as the sum of the factorials of the digits of . For example, 
 #Define  as the sum of the digits of . So .
-
 #Define  to be the smallest positive integer  such that . Though  is ,  is also , and it can be verified that  is .
-
 #Define  as the sum of the digits of . So .
-
 #Further, it can be verified that  is  and  is .
-
 #What is ? As the number can be large, print it modulo .
