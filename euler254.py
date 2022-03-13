a73d8c91a65db886d6f2f1d8f459848019888120
Carlos Isaque Da Silva <85850597+Jenkidamaa@users.noreply.github.com>
2022-03-12 23:58:31 -0300

    Update euler254.py

--- euler254.py
+++ euler254.py
@@ -1,6 +1,6 @@
-#Define  as the sum of the factorials of the digits of . For example, 
-#Define  as the sum of the digits of . So .
-#Define  to be the smallest positive integer  such that . Though  is ,  is also , and it can be verified that  is .
-#Define  as the sum of the digits of . So .
-#Further, it can be verified that  is  and  is .
-#What is ? As the number can be large, print it modulo .
+#Define f(n) as the sum of the factorials of the digits of n. For example, f(342) = 3! + 4! + 2! = 32.
+#Define sf(n) as the sum of the digits of f(n). So sf(342) = 3 + 2 = 5.
+#Define g(i) to be the smallest positive integer n such that sf(n) = i. Though sf(342) is 5, sf(25) is also 5, and it can be verified that g(5) is 25.
+#Define sg(i) as the sum of the digits of g(i). So sg(5) = 2 + 5 = 7.
+#Further, it can be verified that g(20) is 267 and ∑ sg(i) for 1 ≤ i ≤ 20 is 156.
+#What is ∑ sg(i) for 1 ≤ i ≤ 150?
